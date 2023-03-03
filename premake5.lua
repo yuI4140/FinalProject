f03691d538cc326ac921a7734180a8266c4bedad
yuI4140 <102521443+yuI4140@users.noreply.github.com>
2023-03-03 09:42:54 +0100

    init

---
+++ premake5.lua
@@ -0,0 +1,25 @@
+-- Include conan gennerate script
+include("conanbuildinfo.premake.lua")
+
+-- Main Workspace
+workspace "FinalProject"
+    -- Import conan gennerate config
+    conan_basic_setup()
+
+    -- Project
+    project "fp"
+        kind "ConsoleApp"
+        language "C++"
+        targetdir "bin/%{cfg.buildcfg}"
+		objdir "bin/%{cfg.buildcfg}/obj/"
+		location "src"
+        debugdir "app"
+        linkoptions { conan_exelinkflags }
+        files { "**.h", "**.cpp" }
+        filter "configurations:Debug"
+		defines { "DEBUG" }
+		symbols "On"
+
+		filter "configurations:Release"
+		defines { "NDEBUG" }
+		optimize "On"
